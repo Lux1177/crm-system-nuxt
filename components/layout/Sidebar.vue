a55107464d21f1e9e1efd58d7cb7fbaeaa6596fe
Lux1177 <ualimdjanov10@gmail.com>
2024-08-06 22:13:57 +0500

    First commit

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+		<aside class="px-5 py-8 bg-sidebar h-full relative w-full">
+				<NuxtLink to="/" class="mb-10 block">
+						<NuxtImg src="/logo.svg" alt="Logo" width="100px" class="mx-auto" />
+				</NuxtLink>
+
+				<UiButton>Send</UiButton>
+		</aside>
+</template>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+	<section class="grid" style="min-height: 100vh">
+			<aside>
+					<LayoutSidebar />
+			</aside>
+			<div>
+					<slot />
+			</div>
+	</section>
+
+</template>
+
+<style scoped>
+
+.grid {
+		display: grid;
+		grid-template-columns: 1fr 6fr;
+}
+
+</style>
